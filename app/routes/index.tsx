1cb27b4ad79eb7a25ed118dd5b34249ba6c837c4
FannyHasCat <huijuan.95tx@gmail.com>
2022-10-30 21:39:07 +0800

    feat: change dom

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -6,13 +6,15 @@ import Paper from "@mui/material/Paper";
 import Header from "~/components/header";
 import { Typography } from "@mui/material";
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
+const Item = styled(Paper)(({ theme }) => {
+  return {
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  };
+});
 
 const Div = styled(Item)(() => ({
   display: "flex",
@@ -38,12 +40,10 @@ function ResponsiveAppBar() {
             </Div>
           </Grid>
           <Grid item xs={5} md={5}>
-            <Item>
-              <img
-                src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e"
-                alt="图片"
-              />
-            </Item>
+            <img
+              src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e"
+              alt="图片"
+            />
           </Grid>
         </Grid>
       </Box>
